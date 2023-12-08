1a69d3865a37261236658622195066ec5ad057a7
Kienroro2003 <91589673+Kienroro2003@users.noreply.github.com>
2023-12-08 19:40:53 +0700

    update item 16

--- HibernateSpringBootOptional/src/main/java/com/bookstore/service/BookstoreService.java
+++ HibernateSpringBootOptional/src/main/java/com/bookstore/service/BookstoreService.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
-public class BookstoreService {
+public class  BookstoreService {
 
     private final AuthorRepository authorRepository;
     private final BookRepository bookRepository;
